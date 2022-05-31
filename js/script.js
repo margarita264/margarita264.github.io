c3e200732f73c6593e64f1239a5bcba57a08955b
Margarita <44238675+margarita264@users.noreply.github.com>
2022-05-30 21:59:48 -0300

    Corrección para nueva entrega

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function regitrar(){
+    location.href="paginas/registro.html";
+}
+function login(){
+    location.href="paginas/login.html";
+}
+function membresia(){
+    location.href="paginas/membresia.html";
+}
